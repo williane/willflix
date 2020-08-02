b64331459669e57ab52f9421810086c9f93c903b
unknown <williane.tads@gmail.com>
2020-08-02 16:51:08 -0300

    aula 2 final

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import PageDefault from '../../../componentes/PageDefault';
 import { Link } from 'react-router-dom';
 
-function CadatroCategoria(){
-    return(
+function CadatroCategoria() {
+    return (
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
+
+            <form>
+
+                <label>
+                    Nome da Categoria:
+                 <input
+                        type="text"
+                    />
+                </label>
+
+                <button>
+                    Cadastrar
+                </button>
+            </form>
 
             <Link to="/">
                 Ir para Home
