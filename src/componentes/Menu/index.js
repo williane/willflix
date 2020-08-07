1b379504b34088fdf2db399bb26b00bf078b171d
unknown <williane.tads@gmail.com>
2020-08-07 13:04:09 -0300

    Aula4 - Final

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LogoMain from '../../assets/img/LogoMain.png';
+import LogoMain from '../../assets/img/logo.png';
 import './Menu.css';
 import Button from '../Button';
 
-//import ButtonLink from './componets/ButtonLink';
-
+// import ButtonLink from './componets/ButtonLink';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <Link to="/">
-                <img className="Logo" src={LogoMain} alt="Aluraflix" />
-            </Link>
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={LogoMain} alt="Aluraflix" />
+      </Link>
 
-            <Button as={Link} className="ButtonLink" to="/cadastro/video">
-                Novo Video
-            </Button>
-        </nav>
-    );
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo Video
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
